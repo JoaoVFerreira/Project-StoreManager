d369dd96b520d27e7c501f36f1ed0e4bfd7c0eeb
João Ferreira <joaovtf39@gmail.com>
2022-03-03 11:44:05 -0300

    tira variaveis de ambiente do .env e deixa hardcoded, pra ver se passa no github

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,14 +1,14 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-  const QUERY = 'SELECT * FROM products ORDER BY id ASC';
+  const QUERY = 'SELECT * FROM StoreManager.products ORDER BY id ASC';
 
   const [rows] = await connection.execute(QUERY);
   return rows;
 };
 
 const findById = async (id) => {
-  const QUERY = 'SELECT * FROM products WHERE id = ?';
+  const QUERY = 'SELECT * FROM StoreManager.products WHERE id = ?';
 
   const [rows] = await connection.execute(QUERY, [id]);
   return rows;
